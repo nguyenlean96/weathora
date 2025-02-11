5dac927326137da2defae8f2a010c97ad184519f
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-11 09:35:56 -0500

    Indentaion refactoring

--- resources/js/Components/weather-forecast/daily/index.tsx
+++ resources/js/Components/weather-forecast/daily/index.tsx
@@ -4,171 +4,171 @@ import { getDayOfWeek } from '@/Utils';
 import { useWeatherContext } from '@/Context/WeatherDataProvider';
 
 export default function DailyForecast({ width, height, props }: { width: number; height: number; props?: any }) {
-  const {
-    // forecastData,
-    dailyForecastData,
-    isCityLoading,
-    isForecastLoading,
-    currentWeatherError,
-    forecastError
-  }: {
-    dailyForecastData: {
-      list: Array<any>;
-      message?: number;
-      cnt?: number;
-      cod?: string;
-      city?: any;
-    };
-    isCityLoading: boolean;
-    isForecastLoading: boolean;
-    currentWeatherError: any;
-    forecastError: any;
-  } = useWeatherContext();
+    const {
+        // forecastData,
+        dailyForecastData,
+        isCityLoading,
+        isForecastLoading,
+        currentWeatherError,
+        forecastError
+    }: {
+        dailyForecastData: {
+            list: Array<any>;
+            message?: number;
+            cnt?: number;
+            cod?: string;
+            city?: any;
+        };
+        isCityLoading: boolean;
+        isForecastLoading: boolean;
+        currentWeatherError: any;
+        forecastError: any;
+    } = useWeatherContext();
 
-  function getTempMax() {
-    if (dailyForecastData) {
-      return Math.max(...dailyForecastData?.list
-        // Filter out days with max temp only
-        .map((day: { main: any; }) => day.main.temp_max)
-      );
+    function getTempMax() {
+        if (dailyForecastData) {
+            return Math.max(...dailyForecastData?.list
+                // Filter out days with max temp only
+                .map((day: { main: any; }) => day.main.temp_max)
+            );
+        }
+        return -1;
     }
-    return -1;
-  }
-  function getTempMin() {
-    if (dailyForecastData) {
-      return Math.min(...dailyForecastData?.list
-        // Filter out days with min temp only
-        .map((day: { main: any; }) => day.main.temp_min)
-      );
+    function getTempMin() {
+        if (dailyForecastData) {
+            return Math.min(...dailyForecastData?.list
+                // Filter out days with min temp only
+                .map((day: { main: any; }) => day.main.temp_min)
+            );
+        }
+        return -1;
     }
-    return -1;
-  }
 
-  function getTempPos(temp: number) {
-    const tempMax = getTempMax();
-    const tempMin = getTempMin();
-    const tempAvg = tempMax === tempMin ? 0 : ((temp - tempMin) / (tempMax - tempMin));
-    //prettier-ignore
-    return {
-      min: tempMin,
-      max: tempMax,
-      avg: tempAvg,
+    function getTempPos(temp: number) {
+        const tempMax = getTempMax();
+        const tempMin = getTempMin();
+        const tempAvg = tempMax === tempMin ? 0 : ((temp - tempMin) / (tempMax - tempMin));
+        //prettier-ignore
+        return {
+            min: tempMin,
+            max: tempMax,
+            avg: tempAvg,
+        }
     }
-  }
 
-  return (
-    !isCityLoading && !isForecastLoading &&
-    dailyForecastData && (
-      <>
-        <motion.div className="bg-blue-500/80 backdrop-blur-sm rounded-xl w-full p-2 mb-3 grid gap-2"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 1,
-            duration: 0.5,
-          }}
-        >
-          <div className="w-full p-1 border-b">
-            <span className="text-gray-50/80 font-semibold">{String(`Next 6-day forecast`)}</span>
-          </div>
-          <div className='w-full'>
-            {dailyForecastData?.list
-              .sort((a: any, b: any) => parseInt(a.dt) - parseInt(b.dt))
-              .map((day: any, index: number) => (
-                <div className="w-full" key={index}>
-                  {
-                    day?.weather[0] &&
-                    <ForecastDayDisplay
-                      position={index}
-                      isCityLoading={isCityLoading}
-                      isForecastLoading={isForecastLoading}
-                      day={day}
-                      width={width}
-                      height={height}
-                      tempResolve={getTempPos}
-                    />
-                  }
-                </div>
-              ))
-            }
-          </div>
-        </motion.div>
-      </>
+    return (
+        !isCityLoading && !isForecastLoading &&
+        dailyForecastData && (
+            <>
+                <motion.div className="bg-blue-500/80 backdrop-blur-sm rounded-xl w-full p-2 mb-3 grid gap-2"
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{
+                        delay: 1,
+                        duration: 0.5,
+                    }}
+                >
+                    <div className="w-full p-1 border-b">
+                        <span className="text-gray-50/80 font-semibold">{String(`Next 6-day forecast`)}</span>
+                    </div>
+                    <div className='w-full'>
+                        {dailyForecastData?.list
+                            .sort((a: any, b: any) => parseInt(a.dt) - parseInt(b.dt))
+                            .map((day: any, index: number) => (
+                                <div className="w-full" key={index}>
+                                    {
+                                        day?.weather[0] &&
+                                        <ForecastDayDisplay
+                                            position={index}
+                                            isCityLoading={isCityLoading}
+                                            isForecastLoading={isForecastLoading}
+                                            day={day}
+                                            width={width}
+                                            height={height}
+                                            tempResolve={getTempPos}
+                                        />
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
+                </motion.div>
+            </>
+        )
     )
-  )
 }
 
 const ForecastDayDisplay = (props: any) => {
-  const tempRangeBar = useRef<HTMLDivElement>(null);
-  const tempIndicator = useRef<HTMLDivElement>(null);
-  const { position, day, width, height, tempResolve } = props;
-  const [tempPos, setTempPos] = useState<number>(0);
-  const [tempMax, setTempMax] = useState<number>(0);
-  const [tempMin, setTempMin] = useState<number>(0);
-  const [thisTempRange, setThisTempRange] = useState<number>(0);
+    const tempRangeBar = useRef<HTMLDivElement>(null);
+    const tempIndicator = useRef<HTMLDivElement>(null);
+    const { position, day, width, height, tempResolve } = props;
+    const [tempPos, setTempPos] = useState<number>(0);
+    const [tempMax, setTempMax] = useState<number>(0);
+    const [tempMin, setTempMin] = useState<number>(0);
+    const [thisTempRange, setThisTempRange] = useState<number>(0);
 
-  useEffect(() => {
-    const { min, max, avg } = tempResolve(day.main.temp);
-    setTempMax(max);
-    setTempMin(min);
-    setThisTempRange(prev => ((day.main.temp_max - day.main.temp_min) / (max - min)));
-    if (tempRangeBar.current && tempIndicator.current) {
-      // console.log('min', min, 'max', max, 'avg', avg);
-      let indicatorOffset = (avg.toFixed(4) * tempRangeBar.current?.offsetWidth);
+    useEffect(() => {
+        const { min, max, avg } = tempResolve(day.main.temp);
+        setTempMax(max);
+        setTempMin(min);
+        setThisTempRange(prev => ((day.main.temp_max - day.main.temp_min) / (max - min)));
+        if (tempRangeBar.current && tempIndicator.current) {
+            // console.log('min', min, 'max', max, 'avg', avg);
+            let indicatorOffset = (avg.toFixed(4) * tempRangeBar.current?.offsetWidth);
 
-      if (indicatorOffset + tempIndicator.current?.offsetWidth > tempRangeBar.current?.offsetWidth) {
-        indicatorOffset = (indicatorOffset - tempIndicator.current?.offsetWidth) * .99;
-      }
-      setTempPos((prev) => indicatorOffset);
-    }
-  }, [day.main.temp, day.main.temp_max, tempRangeBar.current, tempIndicator.current]);
-
-  return (
-    <div className='grid grid-cols-10 w-full pr-4'>
-      <div className='text-gray-50 col-span-2'>
-        <div className={'px-2 text-base truncate text-ellipsis ' + (position === 0 ? 'font-semibold' : '')}>{getDayOfWeek(day.dt)}</div>
-      </div>
-      <div className='flex items-center justify-center col-span-2 md:col-span-1'>
-        {
-          day?.weather[0]?.icon && (
-            <img className='-translate-y-2' src={`http://openweathermap.org/img/wn/${day?.weather[0]?.icon}.png`} alt={day?.weather[0]?.description} />
-          )
+            if (indicatorOffset + tempIndicator.current?.offsetWidth > tempRangeBar.current?.offsetWidth) {
+                indicatorOffset = (indicatorOffset - tempIndicator.current?.offsetWidth) * .99;
+            }
+            setTempPos((prev) => indicatorOffset);
         }
-      </div>
-      <div className='col-span-6 md:col-span-7'>
-        <div className='hidden bg-gradient-to-r from-blue-600 via-green-500 to-orange-600 w-full h-2 rounded ring-1 ring-white'></div>
-        <div className='grid grid-cols-12 w-full gap-x-2'>
-          <div className='text-blue-100 text-base xl:text-lg text-center font-semibold'>
-            {Math.floor(day.main.temp_min)}
-            &deg;
-          </div>
-          <div className='col-span-10 w-full flex items-center justify-center px-2'>
-            <div className='relative ring-1 ring-white bg-indigo-600/40 h-2 lg:h-2 2xl:h-2.5 rounded-full animate-gradient-x w-full mb-2 translate-y-1'
-              ref={tempRangeBar}
-            >
-              <div ref={tempIndicator}
-                className=' absolute top-0 z-10 rounded-full border border-[#333] bg-[#fff] h-full drop-shadow-sm'
-                style={{
-                  width: (tempIndicator.current ? (tempIndicator.current?.offsetHeight) : '0.6rem'),
-                  marginLeft: `${tempPos}px`,
-                }}
-              ></div>
-              {tempMax !== tempMin && (
-                <div className='hidden top-0 z-0 bg-gradient-to-r from-orange-500 via-orange-500/90 to-orange-500 rounded-full ring-1 ring-white h-full opacity-90'
-                  style={{
-                    width: (thisTempRange * (tempRangeBar.current ? tempRangeBar.current.offsetWidth : 0)).toFixed(2) + 'px',
-                    marginLeft: `${tempPos === 0 ? 0 : (tempPos - ((thisTempRange * (tempRangeBar.current ? tempRangeBar.current.offsetWidth : 0) / 2)))}px`,
-                  }}
-                ></div>
-              )}
+    }, [day.main.temp, day.main.temp_max, tempRangeBar.current, tempIndicator.current]);
+
+    return (
+        <div className='grid grid-cols-10 w-full pr-4'>
+            <div className='text-gray-50 col-span-2'>
+                <div className={'px-2 text-base truncate text-ellipsis ' + (position === 0 ? 'font-semibold' : '')}>{getDayOfWeek(day.dt)}</div>
             </div>
-          </div>
-          <div className='text-orange-100 text-base xl:text-lg text-start font-semibold'>
-            {Math.round(day.main.temp_max)}
-            &deg;
-          </div>
+            <div className='flex items-center justify-center col-span-2 md:col-span-1'>
+                {
+                    day?.weather[0]?.icon && (
+                        <img className='-translate-y-2' src={`http://openweathermap.org/img/wn/${day?.weather[0]?.icon}.png`} alt={day?.weather[0]?.description} />
+                    )
+                }
+            </div>
+            <div className='col-span-6 md:col-span-7'>
+                <div className='hidden bg-gradient-to-r from-blue-600 via-green-500 to-orange-600 w-full h-2 rounded ring-1 ring-white'></div>
+                <div className='grid grid-cols-12 w-full gap-x-2'>
+                    <div className='text-blue-100 text-base xl:text-lg text-center font-semibold'>
+                        {Math.floor(day.main.temp_min)}
+                        &deg;
+                    </div>
+                    <div className='col-span-10 w-full flex items-center justify-center px-2'>
+                        <div className='relative ring-1 ring-white bg-indigo-600/40 h-2 lg:h-2 2xl:h-2.5 rounded-full animate-gradient-x w-full mb-2 translate-y-1'
+                            ref={tempRangeBar}
+                        >
+                            <div ref={tempIndicator}
+                                className=' absolute top-0 z-10 rounded-full border border-[#333] bg-[#fff] h-full drop-shadow-sm'
+                                style={{
+                                    width: (tempIndicator.current ? (tempIndicator.current?.offsetHeight) : '0.6rem'),
+                                    marginLeft: `${tempPos}px`,
+                                }}
+                            ></div>
+                            {tempMax !== tempMin && (
+                                <div className='hidden top-0 z-0 bg-gradient-to-r from-orange-500 via-orange-500/90 to-orange-500 rounded-full ring-1 ring-white h-full opacity-90'
+                                    style={{
+                                        width: (thisTempRange * (tempRangeBar.current ? tempRangeBar.current.offsetWidth : 0)).toFixed(2) + 'px',
+                                        marginLeft: `${tempPos === 0 ? 0 : (tempPos - ((thisTempRange * (tempRangeBar.current ? tempRangeBar.current.offsetWidth : 0) / 2)))}px`,
+                                    }}
+                                ></div>
+                            )}
+                        </div>
+                    </div>
+                    <div className='text-orange-100 text-base xl:text-lg text-start font-semibold'>
+                        {Math.round(day.main.temp_max)}
+                        &deg;
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- resources/js/Components/weather-forecast/hourly/index.tsx
+++ resources/js/Components/weather-forecast/hourly/index.tsx
@@ -2,145 +2,144 @@ import { useEffect, useRef, useState } from 'react';
 import { motion } from 'motion/react';
 import { useWeatherContext } from '@/Context/WeatherDataProvider';
 export default function HourlyForecast() {
-  const { isACityFound, isForecastLoading, hourlyForecastData } = useWeatherContext();
-  const chartDisplayAreaRef = useRef<HTMLDivElement>(null);
-  const [data, setData] = useState<number[]>([]);
-  const [isChartReady, setIsChartReady] = useState<boolean>(false);
+    const { isACityFound, isForecastLoading, hourlyForecastData } = useWeatherContext();
+    const chartDisplayAreaRef = useRef<HTMLDivElement>(null);
+    const [data, setData] = useState<number[]>([]);
+    const [isChartReady, setIsChartReady] = useState<boolean>(false);
 
-  const [chartWidth, setChartWidth] = useState<number | null>(null);
-  const [chartHeight, setChartHeight] = useState<number | null>(null);
-  useEffect(() => {
-    if (hourlyForecastData) {
-      if (hourlyForecastData.list) {
-        setData(hourlyForecastData.list.map((hour: any) => hour.main.temp));
-      }
-    }
-  }, [hourlyForecastData]);
+    const [chartWidth, setChartWidth] = useState<number | null>(null);
+    const [chartHeight, setChartHeight] = useState<number | null>(null);
+    useEffect(() => {
+        if (hourlyForecastData) {
+            if (hourlyForecastData.list) {
+                setData(hourlyForecastData.list.map((hour: any) => hour.main.temp));
+            }
+        }
+    }, [hourlyForecastData]);
 
-  useEffect(() => {
-    // This useEffect waits for the component to fully render, then grabs the ref's dimensions
-    const handleResize = () => {
-      if (chartDisplayAreaRef.current) {
-        setChartWidth(chartDisplayAreaRef.current.scrollWidth || 600);
-        setChartHeight((chartDisplayAreaRef.current.offsetHeight * .76) || 400);
-      }
+    useEffect(() => {
+        // This useEffect waits for the component to fully render, then grabs the ref's dimensions
+        const handleResize = () => {
+            if (chartDisplayAreaRef.current) {
+                setChartWidth(chartDisplayAreaRef.current.scrollWidth || 600);
+                setChartHeight((chartDisplayAreaRef.current.offsetHeight * .76) || 400);
+            }
 
-      console.log('Resized');
-    };
+        };
 
-    // Initially set chart dimensions when the component mounts
-    if (chartDisplayAreaRef.current && !isForecastLoading && isACityFound) {
-      handleResize();
-      setIsChartReady(prev => true); // Once we know the area size, set the chart ready
-    }
-  }, [chartDisplayAreaRef.current, isACityFound, isForecastLoading]);
+        // Initially set chart dimensions when the component mounts
+        if (chartDisplayAreaRef.current && !isForecastLoading && isACityFound) {
+            handleResize();
+            setIsChartReady(prev => true); // Once we know the area size, set the chart ready
+        }
+    }, [chartDisplayAreaRef.current, isACityFound, isForecastLoading]);
 
-  return (
-    <div className={'relative ' + (!isACityFound && isForecastLoading && 'hidden')}>
-      <motion.div className="bg-blue-500/80 backdrop-blur-sm rounded-xl w-full h-fit p-2 text-white mb-3"
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.5,
-          duration: 0.5,
-        }}
-      >
-        <div className="w-full p-1 border-b">
-          <span className="text-white">{String(`Hourly forecast`)}</span>
+    return (
+        <div className={'relative ' + (!isACityFound && isForecastLoading && 'hidden')}>
+            <motion.div className="bg-blue-500/80 backdrop-blur-sm rounded-xl w-full h-fit p-2 text-white mb-3"
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.5,
+                    duration: 0.5,
+                }}
+            >
+                <div className="w-full p-1 border-b">
+                    <span className="text-white">{String(`Hourly forecast`)}</span>
+                </div>
+                <div className='relative overflow-x-auto overflow-y-hidden pb-3 z-10'>
+                    {
+                        hourlyForecastData &&
+                        <div className='z-10 w-full flex items-center justify-evenly ' ref={chartDisplayAreaRef}>
+                            {
+                                hourlyForecastData?.list &&
+                                hourlyForecastData?.list.map((hour: any, index: number) =>
+                                (
+                                    <EachHour key={index} hour={hour} index={index} />
+                                )
+                                )
+                            }
+                        </div>
+                    }
+                    {
+                        !isForecastLoading && isChartReady && chartWidth && chartHeight &&
+                        <div className='absolute left-0 right-0 bottom-2 -z-10'>
+                            <LineChart data={data} width={chartWidth} height={chartHeight} />
+                        </div>
+                    }
+                </div>
+            </motion.div>
         </div>
-        <div className='relative overflow-x-auto overflow-y-hidden pb-3 z-10'>
-          {
-            hourlyForecastData &&
-            <div className='z-10 w-full flex items-center justify-evenly ' ref={chartDisplayAreaRef}>
-              {
-                hourlyForecastData?.list &&
-                hourlyForecastData?.list.map((hour: any, index: number) =>
-                (
-                  <EachHour key={index} hour={hour} index={index} />
-                )
-                )
-              }
-            </div>
-          }
-          {
-            !isForecastLoading && isChartReady && chartWidth && chartHeight &&
-            <div className='absolute left-0 right-0 bottom-2 -z-10'>
-              <LineChart data={data} width={chartWidth} height={chartHeight} />
-            </div>
-          }
-        </div>
-      </motion.div>
-    </div>
-  )
+    )
 }
 
 const EachHour = ({ hour, index }: {
-  hour: any;
-  index: number;
+    hour: any;
+    index: number;
 }): JSX.Element => {
-  const hourTileRef = useRef<HTMLDivElement>(null);
-  return (
-    <div className='min-w-[5rem] p-1 py-2'
-      ref={hourTileRef}
-    >
-      <div className='flex flex-col items-center justify-center text-white bg-white/5 rounded-lg'>
-        <div className='text-sm'>{hour.dt_txt.split(' ')[1].slice(0, 5)}</div>
-        <div className='flex items-center justify-center mb-10'>
-          <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} alt={hour.weather[0].description} className='w-14 h-14 opacity-90' />
+    const hourTileRef = useRef<HTMLDivElement>(null);
+    return (
+        <div className='min-w-[5rem] p-1 py-2'
+            ref={hourTileRef}
+        >
+            <div className='flex flex-col items-center justify-center text-white bg-white/5 rounded-lg'>
+                <div className='text-sm'>{hour.dt_txt.split(' ')[1].slice(0, 5)}</div>
+                <div className='flex items-center justify-center mb-10'>
+                    <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} alt={hour.weather[0].description} className='w-14 h-14 opacity-90' />
+                </div>
+                <div>{Math.round(hour.main.temp).toFixed(0)}&deg;</div>
+            </div>
         </div>
-        <div>{Math.round(hour.main.temp).toFixed(0)}&deg;</div>
-      </div>
-    </div>
-  )
+    )
 }
 
 const LineChart = ({ data, width, height }: {
-  data: number[];
-  width?: number;
-  height?: number;
+    data: number[];
+    width?: number;
+    height?: number;
 }) => {
-  const padding = 40;
-  const [linePath, setLinePath] = useState<string>('');
+    const padding = 40;
+    const [linePath, setLinePath] = useState<string>('');
 
-  const xScale = (value: number, index: number) => {
-    return (index / (data.length - 1)) * ((width ?? 400) - padding * 2) + padding;
-  };
-  const yScale = (value: number, min: number, max: number) => {
+    const xScale = (value: number, index: number) => {
+        return (index / (data.length - 1)) * ((width ?? 400) - padding * 2) + padding;
+    };
+    const yScale = (value: number, min: number, max: number) => {
 
-    return (height ?? 600) - ((value - min) / (max - min)) * ((height ?? 600) - padding * 2) - padding;
-  };
+        return (height ?? 600) - ((value - min) / (max - min)) * ((height ?? 600) - padding * 2) - padding;
+    };
 
-  useEffect(() => {
-    const maxDataValue = Math.max(...data);
-    const minDataValue = Math.min(...data);
-    if (width === undefined || height === undefined) return;
-    // Scale data to fit the width and height
+    useEffect(() => {
+        const maxDataValue = Math.max(...data);
+        const minDataValue = Math.min(...data);
+        if (width === undefined || height === undefined) return;
+        // Scale data to fit the width and height
 
-    // Create the line path for the chart and add a circle at each data point
-    setLinePath(prev => data.map((value, index) =>
-      index === 0
-        ? `M ${xScale(value, index)} ${yScale(value, minDataValue, maxDataValue)}`
-        : `L ${xScale(value, index)} ${yScale(value, minDataValue, maxDataValue)}`
-    ).join(' '))
+        // Create the line path for the chart and add a circle at each data point
+        setLinePath(prev => data.map((value, index) =>
+            index === 0
+                ? `M ${xScale(value, index)} ${yScale(value, minDataValue, maxDataValue)}`
+                : `L ${xScale(value, index)} ${yScale(value, minDataValue, maxDataValue)}`
+        ).join(' '))
 
-  }, [data, width, height]);
+    }, [data, width, height]);
 
-  return (
+    return (
 
-    <svg className='opacity-80' width={width} height={height}>
-      {/* Draw the line */}
-      <path d={linePath} stroke="#ddd" fill="none" strokeWidth="2" />
+        <svg className='opacity-80' width={width} height={height}>
+            {/* Draw the line */}
+            <path d={linePath} stroke="#ddd" fill="none" strokeWidth="2" />
 
-      {/* Draw the dots */}
-      {data.map((value, index) => (
-        <circle
-          key={index}
-          cx={xScale(value, index)}
-          cy={yScale(value, Math.min(...data), Math.max(...data))}
-          r={4}  // Radius of the dot
-          fill="#fff"  // Fill color of the dot
-        />
-      ))}
-    </svg>
-  );
+            {/* Draw the dots */}
+            {data.map((value, index) => (
+                <circle
+                    key={index}
+                    cx={xScale(value, index)}
+                    cy={yScale(value, Math.min(...data), Math.max(...data))}
+                    r={4}  // Radius of the dot
+                    fill="#fff"  // Fill color of the dot
+                />
+            ))}
+        </svg>
+    );
 };

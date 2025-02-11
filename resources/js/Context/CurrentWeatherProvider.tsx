a08698aa08dfdba16b56089405360a8902b98093
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-11 08:14:56 -0500

    Restructured how the data providers wrapping the sites

---
+++ resources/js/Context/CurrentWeatherProvider.tsx
@@ -0,0 +1,26 @@
+import { useWeatherData } from '@/Hooks/useWeather';
+import { useContext, createContext, type PropsWithChildren, useState, useEffect, useMemo } from 'react';
+import { useCity } from './CityProvider';
+
+const CurrentWeatherContext = createContext<any>(null);
+
+export function useCurrentWeather() {
+    const value = useContext(CurrentWeatherContext);
+
+    if (!value) {
+        throw new Error('useCurrentWeather must be used within a CurrentWeatherProvider');
+    }
+
+    return value;
+}
+
+export default function CurrentWeatherProvider({ children }: PropsWithChildren) {
+    const { city } = useCity();
+    const {loading, data, fetcher} = useWeatherData(city);
+
+    return (
+        <CurrentWeatherContext.Provider value={{}}>
+            {children}
+        </CurrentWeatherContext.Provider>
+    );
+}
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,12 +1,13 @@
 import DebugPanel from '@/Components/debug-panel';
 import Main from '@/Components/main';
-import SearchPanel from '@/Components/search-panel';
+import SearchPanel from '@/Components/SearchPanel';
+import CurrentWeatherProvider from '@/Context/CurrentWeatherProvider';
+import ForecastDataProvider from '@/Context/ForecastDataProvider';
 import Guest from '@/Layouts/GuestLayout';
 import { Head } from '@inertiajs/react';
 import { useViewportSize } from '@mantine/hooks';
 
 export default function Dashboard() {
-    const { width, height } = useViewportSize();
     return (
         <Guest>
             <Head title="Dashboard" />
@@ -14,22 +15,18 @@ export default function Dashboard() {
             <div className="relative w-screen h-screen overflow-hidden">
                 <div className="flex flex-col relative md:grid md:grid-cols-5 lg:grid-cols-4">
                     <div className="absolute top-0 left-1/2 -translate-x-1/2 right-0 z-50 md:relative md:z-0 md:col-span-2 lg:col-span-1 md:h-full">
-                        <SearchPanel
-                            width={width}
-                            height={height}
-                        />
+                        <SearchPanel />
                     </div>
-                    <div className="md:col-span-3 md:overflow-hidden">
-                        <Main
-                            width={width}
-                            height={height}
-                        />
-                    </div>
+
+                    <CurrentWeatherProvider>
+                        <ForecastDataProvider>
+                            <div className="md:col-span-3 md:overflow-hidden">
+                                <Main />
+                                <DebugPanel />
+                            </div>
+                        </ForecastDataProvider>
+                    </CurrentWeatherProvider>
                 </div>
-                <DebugPanel
-                    width={width}
-                    height={height}
-                />
             </div>
         </Guest>
     );

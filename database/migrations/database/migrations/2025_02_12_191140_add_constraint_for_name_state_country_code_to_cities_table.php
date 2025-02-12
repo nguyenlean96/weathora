b3452b54ee45372420bff53da503c0ca001a6ec9
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2025-02-12 15:14:33 -0500

    Add unique constraint for combination of city name, state and country (#7)
    * Add unique constraint for combination of city name, state and country
    
    * Filter out the duplicates to comply with the unique constraint

---
+++ database/migrations/database/migrations/2025_02_12_191140_add_constraint_for_name_state_country_code_to_cities_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            // Define composite unique constraint
+            $table->unique(['name', 'state', 'country_code'], 'cities_name_state_country_code_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->dropUnique('cities_name_state_country_code_unique');
+        });
+    }
+};

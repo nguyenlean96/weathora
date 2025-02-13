53fc44b552a9164cf3bb01ebca65a41fd4e08825
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-12 20:52:55 -0500

    Add city string to use as a trigger to retrieve city weather data

--- resources/js/Context/WeatherDataProvider.tsx
+++ resources/js/Context/WeatherDataProvider.tsx
@@ -5,61 +5,38 @@ import {
     type PropsWithChildren,
 } from 'react';
 
-const samples = [
-    'London',
-    'Oshawa',
-    'Sudbury',
-    'Toronto',
-    'Windsor',
-    'Winnipeg',
-    'Halifax',
-    'Hamilton',
-    'Kitchener',
-    'New York',
-    'Paris',
-    'Los Angeles',
-    'Chicago',
-    'Las Vegas',
-    'Berlin',
-    'Amsterdam',
-    'Barcelona',
-    'Vienna',
-    'Prague',
-    'Brussels',
-    'Zurich',
-    'Frankfurt',
-    'Oslo',
-    'Stockholm',
-    'Copenhagen',
-    'Helsinki',
-    'Warsaw',
-    'Krakow',
-    'Gdansk',
-    'Wroclaw',
-    'Poznan',
-    'Katowice',
-    'Gdynia',
-    'Sopot',
-    'Gliwice',
-    'Zakopane',
-    'Zamosc',
-    'Zielona Gora',
-];
+export const WeatherContext = createContext<any>({
+    fetchWeatherData: (city: string) => { },
+    isFogEffectForcedOn: false,
+    isSunFlareEffectForcedOn: false,
+    isRainEffectForcedOn: false,
+});
 
-export const WeatherContext = createContext<any>({});
+// export useWeatherContext
+export function useWeatherContext() {
+    const context = useContext(WeatherContext);
+    if (context === undefined) {
+        throw new Error('useWeatherContext must be used within a WeatherProvider');
+    }
+    return context;
+};
 
 export default function WeatherProvider({ children }: PropsWithChildren) {
-    const foundEntry = samples[Math.floor(Math.random() * samples.length)];
+    const [cityWeather, setCityWeather] = useState<string>('');
 
     const [isFogEffectForcedOn, setIsFogEffectForcedOn] = useState<boolean>(false);
     const [isSunFlareEffectForcedOn, setIsSunFlareEffectForcedOn] = useState<boolean>(false);
     const [isRainEffectForcedOn, setIsRainEffectForcedOn] = useState<boolean>(false);
 
+    const fetchWeatherData = (city: string) => {
+        setCityWeather((_: string) => city);
+    }
 
     return (
         <WeatherContext.Provider
             value={{
-                samples,
+                cityWeather,
+                fetchWeatherData,
                 isFogEffectForcedOn,
                 isSunFlareEffectForcedOn,
                 isRainEffectForcedOn,
@@ -69,14 +46,3 @@ export default function WeatherProvider({ children }: PropsWithChildren) {
         </WeatherContext.Provider>
     );
 }
-
-// export useWeatherContext
-const useWeatherContext = () => {
-    const context = useContext(WeatherContext);
-    if (context === undefined) {
-        throw new Error('useWeatherContext must be used within a WeatherProvider');
-    }
-    return context;
-};
-
-export { useWeatherContext };

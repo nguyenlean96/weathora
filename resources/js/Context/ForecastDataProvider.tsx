4e0b25c96b5746653641260044a45c9ed2160f70
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-13 00:55:45 -0500

    [IMPORTANT] Refactored, added comments for clarification, and destructure the forecast data into hourly and daily data

--- resources/js/Context/ForecastDataProvider.tsx
+++ resources/js/Context/ForecastDataProvider.tsx
@@ -1,13 +1,15 @@
+import { useForecastData } from '@/Hooks';
 import {
     useContext,
     createContext,
     type PropsWithChildren,
     useState, useEffect, useMemo
 } from 'react';
+import { useWeatherContext } from './WeatherDataProvider';
 
 const ForecastDataContext = createContext<any>(null);
 
-export function useForecastData() {
+export function useForecastDataContext() {
     const value = useContext(ForecastDataContext);
 
     if (!value) {
@@ -19,16 +21,14 @@ export function useForecastData() {
 
 
 export default function ForecastDataProvider({ children }: PropsWithChildren) {
-    const [dailyForecastData, setDailyForecastData] = useState<{
-        list: Array<IForecastDateTime>;
-        message?: number;
-        cnt?: number;
-        cod?: string;
-        city?: any;
-    } | null>(null);
-    const [hourlyForecastData, setHourlyForecastData] = useState<any | null>(null);
-    const [forecastError, setForecastError] = useState('');
+    const { cityWeather } = useWeatherContext();
+    const { loading, data } = useForecastData(cityWeather);
 
+    /**
+     *
+     * @param oridat
+     * @returns
+     */
     const UniqueDays = (oridat: {
         list: Array<any>;
         message?: number;
@@ -38,22 +38,48 @@ export default function ForecastDataProvider({ children }: PropsWithChildren) {
     }) => {
         let loadFirstDayData = 0;
         let hourlyData: Array<any> = [];
+        // Get the current day of the week
         let cwday: number = -1;
         let uniqueDays: Array<any> = [];
         oridat.list
+            // First sort the data by date
             .sort((a: { dt: string }, b: { dt: string }) => parseInt(a.dt) - parseInt(b.dt))
-            .forEach((day: { dt: string }) => {
+            .forEach((day: { main: any; dt: string }) => {
+                // Get the day of the week
                 let wday = new Date(parseInt(day.dt) * 1000).getDay();
+                /**
+                 * cwday: Current week day
+                 *
+                 *  - This one is used to keep track of the current day of
+                 *  the week that is being processed while looping through the data
+                 *
+                 * wday: Week day
+                 */
                 if (wday !== cwday) {
                     loadFirstDayData++;
                     cwday = wday;
                     uniqueDays.push(day);
+                } else {
+                    /**
+                     *  If the day is already in the uniqueDays array, then
+                     *  update the max and min temperature of the day to enhance
+                     *  the min and max range rather than just the current temperature
+                     */
+                    uniqueDays[uniqueDays.length - 1].main.temp_max = Math.max(
+                        uniqueDays[uniqueDays.length - 1].main.temp_max,
+                        day.main.temp_max
+                    );
+                    uniqueDays[uniqueDays.length - 1].main.temp_min = Math.min(
+                        uniqueDays[uniqueDays.length - 1].main.temp_min,
+                        day.main.temp_min
+                    );
                 }
                 if (loadFirstDayData < 4) {
                     hourlyData.push(day);
                 }
             });
 
+        // Clone the original data
         let filteredHours: any = new Object({
             ...oridat,
         });
@@ -71,12 +97,20 @@ export default function ForecastDataProvider({ children }: PropsWithChildren) {
         return [filteredHours, filteredDays];
     };
 
-    const fetchData = async () => {
-
-    }
+    const { hourly, daily } = useMemo(() => {
+        if (!loading && data) {
+            const [hourly, daily] = UniqueDays(data);
+            return { hourly, daily };
+        }
+        return { hourly: null, daily: null };
+    }, [data]);
 
     return (
-        <ForecastDataContext.Provider value={{}}>
+        <ForecastDataContext.Provider value={{
+            hourly,
+            daily,
+            loading,
+        }}>
             {children}
         </ForecastDataContext.Provider>
     )

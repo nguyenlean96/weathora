4aba730e279ae4159662abb8dd206ad068242c49
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2025-02-13 14:39:15 -0500

    Fix timezone converter (#19)
    * Add google maps service api and key for location details
    
    * Add locale resolvers and explantion for calculations and their magic number
    
    * Code refactored and applied the locale calculation for displaying the sun/moon accordingly on their axis
    
    * Refactored the getTime function since the data passed through has the locale resolved already
    
    * Minor refactoring

--- resources/js/Components/sun-rise/index.tsx
+++ resources/js/Components/sun-rise/index.tsx
@@ -1,25 +1,40 @@
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import { motion } from "motion/react";
-import { getTime } from "@/Utils";
-import { TheSun } from "@/Components/celestial";
+import { TheSun, TheMoon } from "@/Components/celestial";
 import { useCurrentWeather } from '@/Context/CurrentWeatherProvider';
+import { getTime } from '@/Utils';
 
 export default function SunRise() {
-    const { data: currentWeather }: { data: any } = useCurrentWeather();
+    const {
+        data: currentWeather,
+        percentFromSunrise,
+        sunriseDateTime,
+    }
+        : {
+            data: any;
+            percentFromSunrise: number;
+            sunriseDateTime: Date;
+        }
+        = useCurrentWeather();
+
+    const scaledPercentFromSunrise = useMemo(() => {
+        return (51 - (percentFromSunrise * (51 - 7))) % 100;
+    }, [percentFromSunrise]);
+
     const sunriseDisplayingAreaRef = useRef<HTMLDivElement>(null);
     const sunriseAnimationDivRef = useRef<SVGSVGElement | null>(null);
     return (
-        <div className={"bg-blue-500/80 backdrop-blur-sm rounded-xl w-full " + (currentWeather?.sys?.sunrise ? ' opacity-100 translate-y-0' : ' opacity-0 translate-y-12')}
+        <div className={"bg-blue-500/80 backdrop-blur-sm rounded-xl overflow-hidden w-full " + (currentWeather?.sys?.sunrise ? ' opacity-100 translate-y-0' : ' opacity-0 translate-y-12')}
             ref={sunriseDisplayingAreaRef}
         >
             {
                 currentWeather?.sys?.sunrise ? (
-                    <div className="px-2 w-full h-full overflow-hidden">
-                        <div className="relative w-full h-full">
+                    <div className="w-full h-full overflow-hidden">
+                        <div className="relative w-full h-full px-2">
                             <div className="absolute left-0 top-0 p-1">
-                                <div className="text-2xl text-end lg:text-start"><span className="font-semibold text-white/60">Sun rises</span> <span className="text-2xl text-white/80 ">{getTime(currentWeather?.sys.sunrise)}</span></div>
+                                <div className="text-2xl text-end lg:text-start"><span className="font-semibold text-white/60">Sun rises</span> <span className="text-2xl text-white/80 ">{getTime(sunriseDateTime)}</span></div>
                             </div>
-                            <div className="absolute left-0 top-2/3 w-full border"></div>
+                            <div className="absolute left-0 top-2/3 w-full border-t-2 z-0 bg-gradient-to-b from-slate-600/20 via-slate-800/60 to-gray-900 h-16"></div>
                             <div
                                 style={{
                                     transform: `translateX(${(sunriseDisplayingAreaRef.current && sunriseAnimationDivRef.current) ?
@@ -30,7 +45,7 @@ export default function SunRise() {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-[1rem] left-0 xl:right-0 z-0 opacity-60"
                                     ref={sunriseAnimationDivRef}
                                 >
-                                    <path d="M 260 110 Q 187.5 6, 125 93 T 6 80" stroke="#eee" strokeWidth="3" strokeLinecap="round"
+                                    <path d="M 260 110 Q 187.5 6, 125 93 T 6 80" stroke="rgb(90 224 249 / 0.4)" strokeWidth="3" strokeLinecap="round"
                                         fill="transparent" />
                                 </svg>
                                 <motion.div
@@ -43,15 +58,25 @@ export default function SunRise() {
                                         offsetDistance: '100%',
                                     }}
                                     animate={{
-                                        offsetDistance: '0%',
+                                        offsetDistance: `${scaledPercentFromSunrise}%`,
                                     }}
                                     transition={{
-                                        delay: 3,
-                                        duration: 120,
-                                        repeat: Infinity,
+                                        delay: 1,
+                                        duration: 2,
                                     }}
                                 >
-                                    <TheSun />
+                                    {
+                                        /**
+                                         *  If the sun is between 50% and 93% of the way to sunrise,
+                                         *  or less than 6% of the way to sunrise,
+                                         */
+                                        (
+                                            (scaledPercentFromSunrise > 50 && scaledPercentFromSunrise < 93)
+                                            || scaledPercentFromSunrise < 6
+                                        )
+                                            ? <TheSun />
+                                            : <TheMoon />
+                                    }
                                 </motion.div>
                             </div>
                         </div>
--- resources/js/Utils/Time.ts
+++ resources/js/Utils/Time.ts
@@ -1,63 +1,70 @@
 // hour:minute:second?
 // Example: 12:59:59
-function getTime(time: any, get_seconds: boolean = false): string {
-	const date = new Date(time * 1000);
-	const hours = date.getHours();
-	const minutes = '0' + date.getMinutes();
-	const seconds = '0' + date.getSeconds();
-	return (
-		(hours > 12 ? hours - 12 : hours) +
-		':' +
-		minutes.slice(minutes.length - 2, minutes.length) +
-		(get_seconds ? ':' + seconds.slice(seconds.length - 2, seconds.length) : '')
-	);
+function getTime(date: any, get_seconds: boolean = false): string {
+    const hours = String(
+        '0'
+        + (
+            date.getHours() > 12
+                ? (date.getHours() - 12)
+                : date.getHours()
+        )
+    ).slice(-2);
+
+    const minutes = String('0' + date.getMinutes()).slice(-2);
+    const seconds = String('0' + date.getSeconds()).slice(-2);
+    return (
+        hours +
+        ':' +
+        minutes +
+        (get_seconds ? ':' + seconds : '')
+    );
 }
 
 function secondConverterToTimerFormat(seconds: number): {
-	year?: number;
-	month?: number;
-	week?: number;
-	day?: number;
-	hours: number;
-	minutes: number;
-	seconds: number;
+    year?: number;
+    month?: number;
+    week?: number;
+    day?: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
 } {
-	const year = Math.floor(seconds / 31536000);
-	const month = Math.floor((seconds - year * 31536000) / 2592000);
-	const week = Math.floor((seconds - year * 31536000 - month * 2592000) / 604800);
-	const day = Math.floor(
-		(seconds - year * 31536000 - month * 2592000 - week * 604800) / 86400
-	);
-	const hours = Math.floor(
-		(seconds - year * 31536000 - month * 2592000 - week * 604800 - day * 86400) / 3600
-	);
-	const minutes = Math.floor(
-		(seconds -
-			year * 31536000 -
-			month * 2592000 -
-			week * 604800 -
-			day * 86400 -
-			hours * 3600) /
-			60
-	);
-	const seconds_left = Math.floor(
-		seconds -
-			year * 31536000 -
-			month * 2592000 -
-			week * 604800 -
-			day * 86400 -
-			hours * 3600 -
-			minutes * 60
-	);
-	return {
-		year: year,
-		month: month,
-		week: week,
-		day: day,
-		hours: hours,
-		minutes: minutes,
-		seconds: seconds_left,
-	};
+    const year = Math.floor(seconds / 31536000);
+    const month = Math.floor((seconds - year * 31536000) / 2592000);
+    const week = Math.floor((seconds - year * 31536000 - month * 2592000) / 604800);
+    const day = Math.floor(
+        (seconds - year * 31536000 - month * 2592000 - week * 604800) / 86400
+    );
+    const hours = Math.floor(
+        (seconds - year * 31536000 - month * 2592000 - week * 604800 - day * 86400) / 3600
+    );
+    const minutes = Math.floor(
+        (seconds -
+            year * 31536000 -
+            month * 2592000 -
+            week * 604800 -
+            day * 86400 -
+            hours * 3600) /
+        60
+    );
+    const seconds_left = Math.floor(
+        seconds -
+        year * 31536000 -
+        month * 2592000 -
+        week * 604800 -
+        day * 86400 -
+        hours * 3600 -
+        minutes * 60
+    );
+    return {
+        year: year,
+        month: month,
+        week: week,
+        day: day,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds_left,
+    };
 }
 
 export { getTime, secondConverterToTimerFormat };

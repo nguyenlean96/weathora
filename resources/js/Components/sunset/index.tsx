b6372ce5898cbc5c84b5eccb66a89c69eb683602
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-13 14:35:02 -0500

    Code refactored and applied the locale calculation for displaying the sun/moon accordingly on their axis

--- resources/js/Components/sunset/index.tsx
+++ resources/js/Components/sunset/index.tsx
@@ -1,26 +1,41 @@
-import { useRef } from 'react';
+import { useRef, useMemo } from 'react';
 import { motion } from 'motion/react';
-import { getTime } from '@/Utils';
-import { TheMoon } from '@/Components/celestial';
+import { TheSun, TheMoon } from '@/Components/celestial';
 import { useCurrentWeather } from '@/Context/CurrentWeatherProvider';
+import { getTime } from '@/Utils';
 
 export default function SunSet() {
-    const { data: currentWeather }: { data: any } = useCurrentWeather();
     const sunsetDisplayingAreaRef = useRef<HTMLDivElement>(null);
     const sunsetAnimationDivRef = useRef<SVGSVGElement | null>(null);
 
+    const {
+        data: currentWeather,
+        sunsetDateTime,
+        percentFromSunrise
+    }
+        : {
+            data: any;
+            sunsetDateTime: Date;
+            percentFromSunrise: number;
+        }
+        = useCurrentWeather();
+
+    const scaledPercentFromSunrise = useMemo(() => {
+        return (92 + (percentFromSunrise * (92 - 50))) % 100;
+    }, [percentFromSunrise]);
+
     return (
-        <div className={"bg-blue-500/80 backdrop-blur-sm rounded-xl w-full h-full " + (currentWeather?.sys?.sunset ? ' opacity-100 translate-y-0' : ' opacity-0 translate-y-12')}
+        <div className={"bg-blue-500/80 backdrop-blur-sm rounded-xl overflow-hidden w-full h-full " + (currentWeather?.sys?.sunset ? ' opacity-100 translate-y-0' : ' opacity-0 translate-y-12')}
             ref={sunsetDisplayingAreaRef}
         >
             {
                 currentWeather?.sys?.sunset ? (
-                    <div className="px-2 w-full h-full overflow-hidden">
-                        <div className="relative w-full h-full">
+                    <div className="w-full h-full overflow-hidden">
+                        <div className="relative w-full h-full px-2">
                             <div className="absolute left-0 top-0 p-1">
-                                <div className="text-2xl text-end lg:text-start"><span className="font-semibold text-white/60">Sun sets</span> <span className="text-2xl text-white/80 ">{getTime(currentWeather?.sys.sunset)}</span></div>
+                                <div className="text-2xl text-end lg:text-start"><span className="font-semibold text-white/60">Sun sets</span> <span className="text-2xl text-white/80 ">{getTime(sunsetDateTime)}</span></div>
                             </div>
-                            <div className="absolute left-0 top-2/3 w-full border"></div>
+                            <div className="absolute left-0 top-2/3 w-full border-t-2 z-0 bg-gradient-to-b from-slate-600/20 via-slate-800/60 to-gray-800 h-16"></div>
                             <div
                                 style={{
                                     transform: `translateX(${(sunsetDisplayingAreaRef.current && sunsetAnimationDivRef.current) ?
@@ -31,7 +46,7 @@ export default function SunSet() {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-[2rem] left-0 xl:right-0 z-0 opacity-60"
                                     ref={sunsetAnimationDivRef}
                                 >
-                                    <path d="M 6 90 Q 58.5 6, 135 75 T 270 60" stroke="#eee" strokeWidth="3" strokeLinecap="round" fill="transparent" />
+                                    <path d="M 6 90 Q 58.5 6, 135 75 T 270 60" stroke="rgb(90 224 249 / 0.4)" strokeWidth="3" strokeLinecap="round" fill="transparent" />
                                 </svg>
                                 <motion.div
                                     className="absolute top-[2rem] left-0 z-50"
@@ -40,18 +55,28 @@ export default function SunSet() {
                                         offsetPath: 'path("M 6 90 Q 58.5 6, 135 75 T 270 60")',
                                     }}
                                     initial={{
-                                        offsetDistance: '35%',
+                                        offsetDistance: '0%',
                                     }}
                                     animate={{
-                                        offsetDistance: '100%',
+                                        offsetDistance: `${scaledPercentFromSunrise}%`,
                                     }}
                                     transition={{
-                                        delay: 3,
-                                        duration: 120,
-                                        repeat: Infinity,
+                                        delay: 2,
+                                        duration: 2,
                                     }}
                                 >
-                                    <TheMoon />
+                                    {
+                                        /**
+                                         *  The sun and the moon are displayed
+                                         *  based on the scaledPercentFromSunrise value.
+                                         */
+                                        (
+                                            (scaledPercentFromSunrise > 49 && scaledPercentFromSunrise < 93)
+                                            || scaledPercentFromSunrise < 8
+                                        )
+                                            ? <TheMoon />
+                                            : <TheSun />
+                                    }
                                 </motion.div>
                             </div>
                         </div>

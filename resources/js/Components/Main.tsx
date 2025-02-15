58b51907c1c5ee3d959562d0033d7e52149144e9
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-15 13:27:43 -0500

    Ported the Temperation info panel into its own component also fixed the ui

--- resources/js/Components/Main.tsx
+++ resources/js/Components/Main.tsx
@@ -5,6 +5,7 @@ import { useViewportSize } from '@mantine/hooks';
 import { useCurrentWeather } from "@/Context/CurrentWeatherProvider";
 import WeatherEffectLayer from "./WeatherEffectLayer";
 import DailyLightCycle from "./DailyLightCycle";
+import TempPanel from "./TempPanel";
 
 export default function Main(_props: any) {
     const { width, height } = useViewportSize();
@@ -100,53 +101,7 @@ export default function Main(_props: any) {
                                 currentWeather
                                 &&
                                 <>
-                                    <motion.div className="col-span-2 bg-blue-500/80 backdrop-blur-sm rounded-xl w-full h-full p-2 grid grid-cols-3 py-4 text-white mb-3"
-                                        initial={{ opacity: 0, y: 20 }}
-                                        whileInView={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5 }}
-                                    >
-                                        {/* MAX TEMPERATURE */}
-                                        <div className="text-center">
-                                            <svg
-                                                xmlns='http://www.w3.org/2000/svg'
-                                                width='16'
-                                                height='16'
-                                                fill='currentColor'
-                                                className='bi bi-thermometer-high inline-block'
-                                                viewBox='0 0 16 16'
-                                            >
-                                                <path d='M9.5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585a1.5 1.5 0 0 1 1 1.415z' />
-                                                <path d='M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z' />
-                                            </svg>
-                                            <span className="text-sm mr-1">H:</span>
-                                            <span className="text-base xl:text-lg font-semibold">{Math.round(currentWeather?.main.temp_max)}</span>
-                                            &deg;
-                                        </div>
-
-                                        {/* MIN TEMPERATURE */}
-                                        <div className="text-center">
-                                            <svg
-                                                xmlns='http://www.w3.org/2000/svg'
-                                                width='16'
-                                                height='16'
-                                                fill='currentColor'
-                                                className='bi bi-thermometer-low inline-block'
-                                                viewBox='0 0 16 16'
-                                            >
-                                                <path d='M9.5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585a1.5 1.5 0 0 1 1 1.415z' />
-                                                <path d='M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z' />
-                                            </svg>
-                                            <span className="text-sm mr-1">L:</span>
-                                            <span className="text-base xl:text-lg font-semibold">{Math.round(currentWeather?.main.temp_min)}</span>
-                                            &deg;
-                                        </div>
-
-                                        {/* FEELS LIKE */}
-                                        <h5 className="text-lg xl:text-xl text-center">
-                                            <span className="text-sm mr-1">Feels like:</span>
-                                            <span className="text-base xl:text-lg font-semibold">{Math.round(currentWeather?.main?.feels_like)}&deg;</span>
-                                        </h5>
-                                    </motion.div>
+                                    <TempPanel />
                                     <div className="col-span-3 h-[10rem] flex">
                                         <DailyLightCycle />
                                     </div>

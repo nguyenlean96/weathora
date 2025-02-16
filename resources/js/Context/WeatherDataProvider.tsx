8feb23705bfe76e899c6bec146300f5afe6edbc8
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-16 17:05:17 -0500

    Replaced the cityName string with an object containing city name, lat and lon attributes also a reducer to update the fields accordingly

--- resources/js/Context/WeatherDataProvider.tsx
+++ resources/js/Context/WeatherDataProvider.tsx
@@ -2,11 +2,17 @@ import {
     useState,
     createContext,
     useContext,
+    useReducer,
     type PropsWithChildren,
 } from 'react';
 
 export const WeatherContext = createContext<any>({
-    fetchWeatherData: (city: string) => { },
+    location: {
+        city: null,
+        lat: null,
+        lon: null,
+    },
+    fetchWeatherData: ({ city, lat, lon }: { city: string; lat: number; lon: number }) => { },
     isFogEffectForcedOn: false,
     isSunFlareEffectForcedOn: false,
     isRainEffectForcedOn: false,
@@ -21,21 +27,61 @@ export function useWeatherContext() {
     return context;
 };
 
+const locationResolver = (state: any, action: any) => {
+    switch (action.type) {
+        case 'SET_LOCATION':
+            return {
+                ...state,
+                city: action.city,
+                lat: action.lat,
+                lon: action.lon,
+            };
+        case 'SET_LAT':
+            return {
+                ...state,
+                lat: action.lat,
+            };
+
+        case 'SET_LON':
+            return {
+                ...state,
+                lon: action.lon,
+            };
+        case 'SET_CITY':
+            return {
+                ...state,
+                city: action.city,
+            };
+        case 'RESET_LOCATION':
+            return {
+                city: null,
+                lat: null,
+                lon: null,
+            };
+        default:
+            return state;
+    }
+}
+
 export default function WeatherProvider({ children }: PropsWithChildren) {
-    const [cityWeather, setCityWeather] = useState<string>('');
+    const [location, dispatchLocationResolver] = useReducer(locationResolver, {
+        city: null,
+        lat: null,
+        lon: null,
+    });
 
     const [isFogEffectForcedOn, setIsFogEffectForcedOn] = useState<boolean>(false);
     const [isSunFlareEffectForcedOn, setIsSunFlareEffectForcedOn] = useState<boolean>(false);
     const [isRainEffectForcedOn, setIsRainEffectForcedOn] = useState<boolean>(false);
 
-    const fetchWeatherData = (city: string) => {
-        setCityWeather((_: string) => city);
+    const fetchWeatherData = ({ city, lat, lon }: { city: string; lat: number; lon: number }) => {
+        dispatchLocationResolver({ type: 'SET_LOCATION', lat, lon });
     }
 
     return (
         <WeatherContext.Provider
             value={{
-                cityWeather,
+                location,
                 fetchWeatherData,
                 isFogEffectForcedOn,
                 isSunFlareEffectForcedOn,

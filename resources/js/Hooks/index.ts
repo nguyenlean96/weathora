45e44e97ac4bd1a046a6364a3418a7941b28eecb
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2025-02-17 13:37:41 -0800

    Remove unused components (#36)
    * Safely removed the unused hooks
    
    * Safely removed the sun sets and sun rises components
    
    ---------
    
    Co-authored-by: horizon-team-inc <horizon.team.toronto@gmail.com>

--- resources/js/Hooks/index.ts
+++ resources/js/Hooks/index.ts
@@ -1,14 +1,10 @@
 import { useWeatherData } from "./useWeatherData";
-import { useForecastData } from "./useForecastData";
-import { useCurrentWeatherData } from "./useCurrentWeatherData";
 import useDebounce from "./useDebounce";
 import { useUnsplashImage } from "./useUnsplashImage";
 import useInfiniteLoading from "./useInfiniteLoading";
 
 export {
     useWeatherData,
-    useCurrentWeatherData,
-    useForecastData,
     useDebounce,
     useUnsplashImage,
     useInfiniteLoading,

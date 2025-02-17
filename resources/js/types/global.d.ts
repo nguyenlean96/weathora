94381b1d2153b9b76fc11de29f59033303ef464b
Le An Nguyen <nguyenlean96@gmail.com>
2025-02-17 15:07:52 -0500

    Add interface for data retrieved from openweathermap to enforce type consistency

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -18,6 +18,95 @@ declare global {
         lat: number;
         lon: number;
     };
+
+    interface IMinuteForecast {
+        dt: number;
+        precipitation: number;
+    }
+    interface IWeatherCondition {
+        id: number;
+        main: string;
+        description: string;
+        icon: string;
+    }
+
+    interface ICurrentWeather {
+        dt: number;
+        sunrise: number;
+        sunset: number;
+        temp: number;
+        feels_like: number;
+        pressure: number;
+        humidity: number;
+        dew_point: number;
+        uvi: number;
+        clouds: number;
+        visibility: number;
+        wind_speed: number;
+        wind_deg: number;
+        wind_gust: number;
+        weather: Array<IWeatherCondition>;
+        snow?: {
+            '1h'?: number;
+            '3h'?: number;
+        };
+        rain?: {
+            '1h': number;
+            '3h': number;
+        };
+    }
+
+    interface IHourlyForecast {
+        dt: number;
+        temp: number;
+        feels_like: number;
+        pressure: number;
+        humidity: number;
+        dew_point: number;
+        uvi: number;
+        clouds: number;
+        visibility: number;
+        wind_speed: number;
+        wind_deg: number;
+        wind_gust: number;
+        weather: Array<IWeatherCondition>;
+        pop: number;
+    }
+
+    interface IDailyForecast {
+        dt: number;
+        sunrise: number;
+        sunset: number;
+        moonrise: number;
+        moonset: number;
+        moon_phase: number;
+        summary: string;
+        temp: {
+            day: number;
+            min: number;
+            max: number;
+            night: number;
+            eve: number;
+            morn: number;
+        };
+        feels_like: {
+            day: number;
+            night: number;
+            eve: number;
+            morn: number;
+        };
+        pressure: number;
+        humidity: number;
+        dew_point: number;
+        wind_speed: number;
+        wind_deg: number;
+        wind_gust: number;
+        weather: Array<IWeatherCondition>;
+        clouds: number;
+        pop: number;
+        uvi: number;
+    }
+
     interface IForecastDateTime {
         dt: number;
         dt_txt: string;
